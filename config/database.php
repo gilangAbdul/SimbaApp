c730c1ea9c84a70a2fe604e9d210b8d38972596b
Gilang Abdul Jabbar <131242348+gilangAbdul@users.noreply.github.com>
2023-12-16 19:16:38 +0700

    balik awal

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('simbabps-server.mysql.database.azure.com', 'simbabps-server.mysql.database.azure.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('simbabps-database', 'simbabps-database'),
-            'username' => env('zjhnmzepqu', 'zjhnmzepqu'),
-            'password' => env('U2V84404B320F7TB$', 'U2V84404B320F7TB$'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => '/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem',
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
